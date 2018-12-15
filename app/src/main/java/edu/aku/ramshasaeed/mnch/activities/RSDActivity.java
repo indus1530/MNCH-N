bac12b11037ac6dc01cdf40f2408572423db15de
ramsha.ahmed <ramshasaeed2016@gmail.com>
2018-12-15 17:37:07 +0500

    Initial commit

---
+++ app/src/main/java/edu/aku/ramshasaeed/mnch/activities/RSDActivity.java
@@ -0,0 +1,33 @@
+package edu.aku.ramshasaeed.mnch.activities;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import org.json.JSONException;
+
+import edu.aku.ramshasaeed.mnch.R;
+import edu.aku.ramshasaeed.mnch.databinding.ActivityRsdBinding;
+
+public class RSDActivity extends AppCompatActivity {
+    //Routine Service Delivery
+    ActivityRsdBinding bi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_rsd);
+        bi.setCallback(this);
+    }
+
+
+    public void BtnContinue() {
+
+    }
+    public void BtnEnd() {
+
+    }
+
+}
